8eade03b04efbfd9a10ca82866301b985e6ff68e
Nkazimulo Siyaya <nkazimulo.siyaya@kcpivot.co.za>
2022-03-30 08:58:26 +0200

    removed test cases

--- FirstAutoTest/TestCases.cs
+++ FirstAutoTest/TestCases.cs
@@ -17,21 +17,22 @@ namespace FirstAutoTest
         private const string Url = "http://automationpractice.com/index.php";
 
         private IWebElement contactUsLink => driver.FindElement(By.CssSelector("div#contact-link"));
-        private IWebElement emailInput => driver.FindElement(By.Id("email"));
-        private IWebElement subjectHeadingDropdown => driver.FindElement(By.Id("id_contact"));
+        private IWebElement emailTextBox => driver.FindElement(By.Id("email"));
         private IWebElement orderReference => driver.FindElement(By.Id("id_order"));
-        private IWebElement message => driver.FindElement(By.Id("message"));
+        private IWebElement messagetextArea => driver.FindElement(By.Id("message"));
         private IWebElement submitMessage => driver.FindElement(By.Id("submitMessage"));
-        private IWebElement emailSentSuccessfully => driver.FindElement(By.CssSelector("p.alert.alert-success"));
-        private IWebElement searchInput => driver.FindElement(By.Name("search_query"));
+        private IWebElement emailSentSuccessfullyMessage => driver.FindElement(By.CssSelector("p.alert.alert-success"));
+        private IWebElement searchTextBox => driver.FindElement(By.Name("search_query"));
         private IWebElement searchButton => driver.FindElement(By.Name("submit_search"));
         private IWebElement centerColumn => driver.FindElement(By.Id("center_column"));
         private List<IWebElement> SearchItemNames => new List<IWebElement>(centerColumn.FindElements(By.CssSelector("ul.product.list.grid.row")));
-        private IWebElement passwordInput => driver.FindElement(By.Id("passwd"));
+        private IWebElement passwordTextBox => driver.FindElement(By.Id("passwd"));
         private IWebElement SubmitLogin => driver.FindElement(By.Id("SubmitLogin"));
         private IWebElement myAccountLabel => driver.FindElement(By.CssSelector("h1.page-heading"));
         private IWebElement signInLink => driver.FindElement(By.CssSelector("a.login"));
         private IWebElement signOutLink => driver.FindElement(By.CssSelector("a.logout"));
+        private IWebElement subjectHeading => driver.FindElement(By.Id("id_contact"));
+        private SelectElement subjectHeadingDropdown => new SelectElement(subjectHeading);
         bool isLoggedIn = false;
 
         [SetUp]
@@ -46,34 +47,34 @@ namespace FirstAutoTest
         }
 
         [Test]
-        public void GivenRequiredData_WhenSendingEmailViaContactUsPage_ThenReturnEmailSuccesfullySent()
+        public void GivenRequiredData_WhenSendingEmailViaContactUsPage_ThenEmailSentSuccessfulMessageReturned()
         {
             contactUsLink.Click();
-            subjectHeadingDropdown.FindElement(By.XPath("//option[. = 'Customer service']")).Click();
-            emailInput.SendKeys("test@test.com");
+            subjectHeadingDropdown.SelectByText("Customer service");
+            emailTextBox.SendKeys("test@test.com");
             orderReference.SendKeys("101");
-            message.SendKeys("My first selenium test");
+            messagetextArea.SendKeys("My first selenium test");
             submitMessage.Click();
 
-            Assert.IsTrue(emailSentSuccessfully.Displayed);
+            Assert.IsTrue(emailSentSuccessfullyMessage.Displayed);
         }
 
         [Test]
-        public void GivenLoginValidCredentials_WhenLoging_ThenReturnSuccessfullyLoggedIn()
+        public void GivenLoginValidCredentials_WhenLoging_ThenMyAccountPageReturned()
         {
             signInLink.Click();
-            emailInput.SendKeys("nka@gmail.com");
-            passwordInput.SendKeys("Nk@Zee");
+            emailTextBox.SendKeys("nka@gmail.com");
+            passwordTextBox.SendKeys("Nk@Zee");
             SubmitLogin.Click();
             isLoggedIn = true;
+
             Assert.IsTrue(myAccountLabel.Text.Equals("MY ACCOUNT"));
         }
 
-
         [Test]
-        public void GivenItemName_WhenSearchingForItem_ThenReturnSearchResults()
+        public void GivenItemName_WhenSearchingForItem_ThenSearchResultsReturned()
         {
-            searchInput.SendKeys("Printed dress");
+            searchTextBox.SendKeys("Printed dress");
             searchButton.Click();
 
             foreach (var itemResult in SearchItemNames)
